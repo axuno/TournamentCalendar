03b9bc332367cf0382e966a8e0192833a0ea5d34
axunonb <nb@axuno.net>
2023-02-27 11:22:28 +0100

    Update NLog configuration
    Update "orga" views

--- TournamentCalendar/WebAppStartup.cs
+++ TournamentCalendar/WebAppStartup.cs
@@ -237,7 +237,7 @@ public static class WebAppStartup
         #region * Setup error handling *
 
         // Error handling must be one of the very first things to configure
-        if (!env.IsProduction())
+        if (env.IsProduction())
         {
             // The StatusCodePagesMiddleware should be one of the earliest 
             // middleware in the pipeline, as it can only modify the response 
