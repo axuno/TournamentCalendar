axuno/TournamentCalendar

commit 013976605f102326773bc9d387ecd0d971a6d5a9

Update TempusDominus datepicker pkg to v6.7.10 (#36)
* Update Site.TempusDominusFactory for less bug fixes
* Update Calendar.Edit view to use CurrentUICulture instead of CurrentCulture

NLog:
* Replace obsolete NLogBuilder.ConfigureNLog with LogManager.Setup().LoadConfigurationFromFile(...)

TournamentCalendar/Program.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using NLog;
 using NLog.Web;
 using NLog.Extensions.Logging;
 
@@ -25,10 +26,10 @@ public class Program
     {
         // NLog: setup the logger first to catch all errors
         var currentDir = Directory.GetCurrentDirectory();
-        var logger = NLogBuilder
-            .ConfigureNLog($@"{currentDir}{Path.DirectorySeparatorChar}{Program.ConfigurationFolder}{Path.DirectorySeparatorChar}NLog.Internal.config")
+        var logger = LogManager.Setup()
+            .LoadConfigurationFromFile(
+                $@"{currentDir}{Path.DirectorySeparatorChar}{Program.ConfigurationFolder}{Path.DirectorySeparatorChar}NLog.Internal.config")
             .GetCurrentClassLogger();
-
         // Allows for <target name="file" xsi:type="File" fileName = "${var:logDirectory}logfile.log"... >
         NLog.LogManager.Configuration.Variables["logDirectory"] = currentDir + Path.DirectorySeparatorChar;
 
@@ -44,8 +45,9 @@ public class Program
             builder.Logging.ClearProviders();
             // Enable NLog as logging provider for Microsoft.Extension.Logging
             builder.Logging.AddNLog(loggingConfig);
-            NLogBuilder.ConfigureNLog(Path.Combine(builder.Environment.ContentRootPath, ConfigurationFolder,
-                $"NLog.{builder.Environment.EnvironmentName}.config"));
+            LogManager.Setup()
+                .LoadConfigurationFromFile(
+                    $"NLog.{builder.Environment.EnvironmentName}.config");
 
             builder.WebHost.ConfigureServices(WebAppStartup.ConfigureServices);
 

TournamentCalendar/WebAppStartup.cs
@@ -174,9 +174,9 @@ public static class WebAppStartup
                 options.Settings = Settings.Deserialize(
                     Path.Combine(context.HostingEnvironment.ContentRootPath, Program.ConfigurationFolder,
                         $@"MailMergeLib.{context.HostingEnvironment.EnvironmentName}.config"),
-                    Encoding.UTF8);
+                    Encoding.UTF8)!;
                 var fms = FileMessageStore.Deserialize(Path.Combine(context.HostingEnvironment.ContentRootPath, Program.ConfigurationFolder,
-                    "MailMergeLibMessageStore.config"), Encoding.UTF8);
+                    "MailMergeLibMessageStore.config"), Encoding.UTF8)!;
                 for (var i = 0; i < fms.SearchFolders.Length; i++)
                 {
                     // make relative paths absolute - ready to use

TournamentCalendar/Library/Mailer.cs
@@ -18,7 +18,6 @@ public class Mailer
     private readonly string _domainName; 
 
     private readonly IMailMergeService _mailMergeService;
-    private readonly MailMergeAddress _contactFormFrom = new(MailAddressType.From, "Volleyball-Turnier.de", "info@volleyball-turnier.de");
 
     public Mailer(IMailMergeService mailMergeService, string domainName)
     {
@@ -28,7 +27,7 @@ public class Mailer
 
     public async Task<ContactModel> ContactForm(ContactModel model, string contactFormUrl)
     {
-        var mmm = _mailMergeService.MessageStore.ScanForMessages().First(m => m.Category == "ContactForm").LoadMessage();
+        var mmm = _mailMergeService.MessageStore.ScanForMessages().First(m => m.Category == "ContactForm").LoadMessage()!;
 
         var so = (model,
             new Dictionary<string, string>
@@ -75,7 +74,7 @@ public class Mailer
         model.EmailSuccessful = false;
         try
         {
-            var mmm = _mailMergeService.MessageStore.ScanForMessages().First(m => m.Category == "CalenderEntry").LoadMessage();
+            var mmm = _mailMergeService.MessageStore.ScanForMessages().First(m => m.Category == "CalenderEntry").LoadMessage()!;
             await _mailMergeService.Sender.SendAsync(mmm, so);
             model.EmailSuccessful = true;
         }
@@ -105,7 +104,7 @@ public class Mailer
         model.EmailSuccessful = false;
         try
         {
-            var mmm = _mailMergeService.MessageStore.ScanForMessages().First(m => m.Category == "InfoServiceEntry").LoadMessage();
+            var mmm = _mailMergeService.MessageStore.ScanForMessages().First(m => m.Category == "InfoServiceEntry").LoadMessage()!;
             await _mailMergeService.Sender.SendAsync(mmm, so);
             model.EmailSuccessful = true;
         }

TournamentCalendar/Scripts/Site.TempusDominusFactory.js
@@ -45,6 +45,18 @@ Site.TempusDominusFactory = class {
                 clear: 'bi bi-trash',
                 close: 'bi bi-x bi-lg'
             };
+        } else {
+            tempusDominus.DefaultOptions.display.icons = {
+                time: 'fas fa-clock',
+                date: 'fas fa-calendar',
+                up: 'fas fa-arrow-up',
+                down: 'fas fa-arrow-down',
+                previous: 'fas fa-chevron-left',
+                next: 'fas fa-chevron-right',
+                today: 'fas fa-calendar-check',
+                clear: 'fas fa-trash',
+                close: 'fas fa-times',
+            };
         }
         tempusDominus.DefaultOptions.display.buttons = {
             today: false,
@@ -66,6 +78,12 @@ Site.TempusDominusFactory = class {
         }
 
         try {
+            try {
+                // Destroy any existing widget
+                if (element._tempusDominus) element._tempusDominus.dispose();
+            }
+            catch {}
+            
             element._tempusDominus = this._tryCreateCalendarPicker(element, format);
         } catch {
             element.querySelector('input').value = '';
@@ -86,6 +104,12 @@ Site.TempusDominusFactory = class {
         }
 
         try {
+            try {
+                // Destroy any existing widget
+                if (element._tempusDominus) element._tempusDominus.dispose();
+            }
+            catch {}
+            
             element._tempusDominus = this._tryCreateTimePicker(element, format);
         } catch {
             element.querySelector('input').value = '';
@@ -175,13 +199,12 @@ Site.TempusDominusFactory = class {
         });
 
         this._fixUncaughtExceptions(timePicker);
-        this._fixMeridiemButtonType(element);
         return timePicker;
     }
 
 
     /**
-    * TempusDominus v6.7.7 throws when invalid dates/times
+    * TempusDominus v6.7.10 throws when invalid dates/times
     * are entered. This workaround catches the exception by
     * overriding the default 'parseInput' method.
     * See: https://github.com/Eonasdan/tempus-dominus/discussions/2656#discussioncomment-5713755
@@ -198,21 +221,5 @@ Site.TempusDominusFactory = class {
                 widget.dates.clear();
             }
         };
-    }
-
-    /**
-    * TempusDominus v6.7.7 does not set the 'type=button' attribute
-    * for the "toggle meridiem button" of the time picker.
-    * Without this type, clicking the button submits the form where the button is located.
-    * See: https://github.com/Eonasdan/tempus-dominus/issues/2820
-    * @param {HTMLElement} element - The element used to create the TempusDominus instance.
-    */
-    _fixMeridiemButtonType(element) 
-    {
-        element.addEventListener('show.td', function () {
-            const container = this._tempusDominus.optionsStore.options.container;
-            const btn = container.querySelector('button[data-action="toggleMeridiem"]')
-            if (btn) btn.setAttribute('type', 'button');
-        });
     }
 }

TournamentCalendar/Views/Calendar/Edit.cshtml
@@ -408,14 +408,14 @@
             });
         }
         
-        const locale ='@(CultureInfo.CurrentCulture.ToString())';
-        const parentLocale = '@(CultureInfo.CurrentCulture.Parent.TwoLetterISOLanguageName)';
-        const hourCycle = '@(CultureInfo.CurrentCulture.DateTimeFormat.ShortTimePattern.Contains("t") ? 12 : 24)';
+        const locale ='@(CultureInfo.CurrentUICulture.ToString())';
+        const parentLocale = '@(CultureInfo.CurrentUICulture.Parent.TwoLetterISOLanguageName)';
+        const hourCycle = '@(CultureInfo.CurrentUICulture.DateTimeFormat.ShortTimePattern.Contains("t") ? 12 : 24)';
         
         const tdFactory = new Site.TempusDominusFactory(locale, parentLocale, hourCycle, true);
 
         [].forEach.call(document.querySelectorAll('[data-input-type="date"]'), function (el) {
-            tdFactory.CreateCalendarPicker(el, '@(CultureInfo.CurrentCulture.DateTimeFormat.ShortDatePattern)');
+            tdFactory.CreateCalendarPicker(el, '@(CultureInfo.CurrentUICulture.DateTimeFormat.ShortDatePattern)');
             el._tempusDominus.updateOptions({
                 restrictions: {
                     minDate: new tempusDominus.DateTime().manipulate(2, 'date').startOf('date'), // +2 days
@@ -425,7 +425,7 @@
         });
 
         [].forEach.call(document.querySelectorAll('[data-input-type="time"]'), function (el) {
-            tdFactory.CreateTimePicker(el, '@(CultureInfo.CurrentCulture.DateTimeFormat.ShortTimePattern)');
+            tdFactory.CreateTimePicker(el, '@(CultureInfo.CurrentUICulture.DateTimeFormat.ShortTimePattern.Replace("tt", "T"))');
         });
 
         tdFactory.SetEscapeKeyClosesPicker(document.querySelectorAll('[data-input-type]'));

commit 57c3ea87e26726bf8305974218bd766906143bad

Fix: Logging configuration
Was broken after #36

Data/DbContext.cs
@@ -11,13 +11,12 @@ namespace TournamentCalendar.Data;
 public class DbContext : IDbContext
 {
     private readonly object _locker = new();
-    private bool _cacheIsRegistered = false;
 
     public DbContext()
     {
         AppDb = new AppDb(this);
     }
-       
+
     /// <summary>
     /// The connection key used to retrieve the <see cref="ConnectionString"/>.
     /// </summary>
@@ -37,7 +36,7 @@ public class DbContext : IDbContext
     /// The schema inside the database.
     /// </summary>
     public virtual string Schema { get; set; } = string.Empty;
-        
+
     /// <summary>
     /// Gets or sets the timeout value to use with the command object(s) created by <see cref="IDataAccessAdapter"/>s.
     /// Default is 30 seconds
@@ -55,15 +54,11 @@ public class DbContext : IDbContext
     {
         lock (_locker)
         {
-            if (!_cacheIsRegistered)
-            {
-                // see docs: https://www.llblgen.com/Documentation/5.9/LLBLGen%20Pro%20RTF/Using%20the%20generated%20code/gencode_resultsetcaching.htm
-                // if connection string exists, the method simply returns without creating a new cache
-                // ** Note ** The connection string must be EXACTLY as it's used for queries.
-                CacheController.RegisterCache(ConnectionString, new ResultsetCache(TimeSpan.FromDays(1).Seconds));
-                _cacheIsRegistered = true;
-            }
-                
+            // see docs: https://www.llblgen.com/Documentation/5.9/LLBLGen%20Pro%20RTF/Using%20the%20generated%20code/gencode_resultsetcaching.htm
+            // if connection string exists, the method simply returns without creating a new cache
+            // ** Note ** The connection string must be EXACTLY as it's used for queries.
+            CacheController.RegisterCache(ConnectionString, new ResultsetCache(TimeSpan.FromDays(1).Seconds));
+
             return new DataAccessAdapter(ConnectionString)
             {
                 KeepConnectionOpen = true,
@@ -85,7 +80,7 @@ public class DbContext : IDbContext
             };
         }
     }
-        
+
     /// <summary>
     /// Gives access to the repositories.
     /// </summary>

TournamentCalendar/Program.cs
@@ -26,9 +26,10 @@ public class Program
     {
         // NLog: setup the logger first to catch all errors
         var currentDir = Directory.GetCurrentDirectory();
+        var logConfigFile = Path.Combine(currentDir, ConfigurationFolder, "NLog.Internal.config");
+
         var logger = LogManager.Setup()
-            .LoadConfigurationFromFile(
-                $@"{currentDir}{Path.DirectorySeparatorChar}{Program.ConfigurationFolder}{Path.DirectorySeparatorChar}NLog.Internal.config")
+            .LoadConfigurationFromFile(logConfigFile)
             .GetCurrentClassLogger();
         // Allows for <target name="file" xsi:type="File" fileName = "${var:logDirectory}logfile.log"... >
         NLog.LogManager.Configuration.Variables["logDirectory"] = currentDir + Path.DirectorySeparatorChar;
@@ -41,13 +42,15 @@ public class Program
                 
             var builder = SetupBuilder(args);
 
-            var loggingConfig = builder.Configuration.GetSection("Logging");
             builder.Logging.ClearProviders();
             // Enable NLog as logging provider for Microsoft.Extension.Logging
-            builder.Logging.AddNLog(loggingConfig);
-            LogManager.Setup()
-                .LoadConfigurationFromFile(
-                    $"NLog.{builder.Environment.EnvironmentName}.config");
+            logConfigFile = Path.Combine(currentDir, ConfigurationFolder, $"NLog.{builder.Environment.EnvironmentName}.config");
+            var nLogConfiguration = LogManager.Setup()
+                .LoadConfigurationFromFile(logConfigFile)
+                .LogFactory.Configuration;
+            var nLogOptions = new NLogAspNetCoreOptions { AutoShutdown = true, IncludeScopes = true };
+            builder.Logging.AddNLog(nLogConfiguration, nLogOptions);
+            builder.Host.UseNLog();
 
             builder.WebHost.ConfigureServices(WebAppStartup.ConfigureServices);
 

TournamentCalendar/TournamentCalendar.csproj
@@ -3,8 +3,8 @@
 	<PropertyGroup>
         <Product>TournamentCalendar</Product>
         <TargetFramework>net6.0</TargetFramework>
-        <Version>3.2.2</Version>
-        <FileVersion>3.2.2</FileVersion>
+        <Version>3.2.3</Version>
+        <FileVersion>3.2.3</FileVersion>
         <EnableDefaultContentItems>true</EnableDefaultContentItems>
         <Authors>axuno gGmbH</Authors>
         <CurrentYear>$([System.DateTime]::Now.ToString(yyyy))</CurrentYear>

TournamentCalendar/WebAppStartup.cs
@@ -249,8 +249,6 @@ public static class WebAppStartup
 
         #region * Setup error handling *
 
-       
-
         // Error handling must be one of the very first things to configure
         if (env.IsProduction())
         {